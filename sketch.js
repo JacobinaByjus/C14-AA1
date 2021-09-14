490331bc288306150477eed03d93d23c95000e04
JacobinaByjus <jacobinalarissa@gmail.com>
2021-09-14 11:57:00 -0300

    vs1

--- sketch.js
+++ sketch.js
@@ -1,26 +1,20 @@
 var ball = {
-  x:20,
-  y:30,
-  r:30,
-  xspeed:0,
-  yspeed:0, 
-  color:["blue","red","green","purple"],
- 
+  x: 20,
+  y: 30,
+  r: 30,
+  xSpeed: 0,
+  ySpeed: 0,
+  color: ["blue", "red", "green", "purple"],
 };
 
-function setup() 
-{
+function setup() {
   createCanvas(400, 400);
- 
 }
 
-function draw() 
-{
+function draw() {
   background(220);
-  circle(ball.x,ball.y,ball.r);
+  circle(ball.x, ball.y, ball.r);
   fill(ball.color[0]);
-  ball.xspeed = 1;
-  ball.x= ball.x + ball.xspeed;
-    
+  ball.xSpeed = 1;
+  ball.x = ball.x + ball.xSpeed;
 }
-
